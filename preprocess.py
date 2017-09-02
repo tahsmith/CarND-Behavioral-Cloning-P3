dacda7f836440227594a9192e52c97abd9e50bb9
Timothy Smith <tsmith@saberastro.com>
2017-09-02 13:51:17 +1000

    Dave2

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,27 @@
+from itertools import zip_longest
 from math import ceil
 import sys
+
+from sklearn.utils import shuffle
 
 from model import *
 from pickle import dump
 from operator import itemgetter
 
-batch_size = 128
+batch_size = 2 ** 10
 columns = load_driving_log('data/driving_log.csv')
 
 validation_ratio = 0.2
 total_records = columns[0].shape[0]
 
 columns = shuffle(*columns)
+
+
+def grouper(iterable, n, fillvalue=None):
+    """Collect data into fixed-length chunks or blocks"""
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
 
 validation_set_count = int(ceil(total_records * validation_ratio))
 validation_set = list(map(itemgetter(slice(0, validation_set_count)), columns))
@@ -26,15 +36,13 @@ if not os.path.isdir('data_cache'):
     os.mkdir('data_cache')
 
 augmented_training_set_count = 0
-for i in range(training_set_count // batch_size + 1):
-    end = min((i + 1) * batch_size, training_set_count)
-    left, center, right, steering = map(itemgetter(slice(i * batch_size, end)),
-                                        training_set)
-    images, steering = make_training_batch(left, center, right, steering)
+for i, batch in enumerate(grouper(generate_training_points(zip(*training_set)), batch_size)):
+    images, steering = zip(*batch)
+    images = np.stack(images)
+    steering = np.array(steering)
     with open('data_cache/training-{}.p'.format(i), 'wb') as file:
         dump((images, steering), file)
-
-    sys.stdout.write('Processed {} of {}\n'.format(end, training_set_count))
+    sys.stdout.write('Processed {}\n'.format(augmented_training_set_count))
     sys.stdout.flush()
     augmented_training_set_count += steering.shape[0]
 
