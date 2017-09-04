0b0874ab02b69afbeafd9b874f489bbd80c7487c
Timothy Smith <tsmith@saberastro.com>
2017-09-04 10:51:26 +1000

    Dave2 nvidia model

--- model.py
+++ model.py
@@ -12,38 +12,21 @@ import cv2
 from pickle import load
 import sklearn.utils
 
+
 def build_model():
     image_shape = (85, 240, 3)
     model = Sequential()
     model.add(InputLayer(input_shape=image_shape))
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-
+    model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Conv2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Conv2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Conv2D(64, 3, 3, activation='relu'))
+    model.add(Conv2D(64, 3, 3, activation='relu'))
     model.add(Flatten())
-    model.add(Dense(1200, activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(50, activation='sigmoid'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(10, activation='sigmoid'))
-    model.add(Dropout(0.25))
-
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
     model.add(Dense(1))
 
     return model
